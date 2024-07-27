60c0f9cc015142e09fdd18c56021fbd6fbd712d5
nexondevfromvietnam <khorryvfahnifa@tmt.vitik.edu.vn>
2024-07-27 15:03:39 +0700

    Add files via upload

--- resources/jscript/main.js
+++ resources/jscript/main.js
@@ -2,3 +2,8 @@ var host = "ndqm.site";
     if ((host == window.location.host) && (window.location.protocol != "https:")){
         window.location.protocol = "https";
     }
+
+//remove hash at the end of the url when user reload
+if(window.location.hash){
+    location.href = './index.html'
+};
